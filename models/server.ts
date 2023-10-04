b6a3bd76753a34594c51b7f947b732351f927a29
EdsonJSF <edsonjsf1990@gmail.com>
2023-10-03 22:34:31 -0400

    Feat: user routes

--- models/server.ts
+++ models/server.ts
@@ -1,12 +1,23 @@
 import express, { Application } from "express";
 
+import userRoutes from "../routes/user.routes";
+
 class Server {
   private app: Application;
   private port: string;
+  private paths = {
+    users: "/api/users",
+  };
 
   constructor() {
     this.app = express();
     this.port = process.env.PORT ?? "8000";
+
+    this.routes();
+  }
+
+  routes() {
+    this.app.use(this.paths.users, userRoutes);
   }
 
   listen() {
